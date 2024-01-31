ebf7b85962ebe89d3f808d8ef2b691899fceeaa3
nichita <nichita.saharov@iongroup.com>
2024-01-31 10:35:34 +0200

    new step to move file after being processed added

--- src/main/java/org/example/config/SpringBatchConfig.java
+++ src/main/java/org/example/config/SpringBatchConfig.java
@@ -8,12 +8,14 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,11 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Configuration
 @EnableBatchProcessing
@@ -39,7 +46,7 @@ public class SpringBatchConfig {
     @Bean
     public FlatFileItemReader<EnterpriseSurvey> reader() {
         FlatFileItemReader<EnterpriseSurvey> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new FileSystemResource("src/main/resources/enterprise-survey-csv.csv"));
+        itemReader.setResource(new FileSystemResource("src/main/resources/unprocessed/enterprise-survey-csv.csv"));
         itemReader.setName("csvReader");
         itemReader.setLinesToSkip(1);
         itemReader.setLineMapper(lineMapper());
@@ -54,7 +61,7 @@ public class SpringBatchConfig {
         lineTokenizer.setDelimiter(",");
         lineTokenizer.setStrict(false);
         lineTokenizer.setNames(
-                "Year", "Industry_aggregation", "Industry_code", "Industry_name", "Units", "Variable_code", "Variable_name", "Variable_category", "Value", "Industry_code"
+                "Year", "Industry_aggregation", "Industry_code", "Industry_name", "Units", "Variable_code", "Variable_name", "Variable_category", "Value", "Industry_code_desc"
         );
 
         BeanWrapperFieldSetMapper<EnterpriseSurvey> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
@@ -81,7 +88,7 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public Step step1() {
+    public Step processFileStep() {
         return new StepBuilder("csv-step", jobRepository)
                 .<EnterpriseSurvey, EnterpriseSurvey>chunk(100, transactionManager)
                 .reader(reader())
@@ -92,9 +99,28 @@ public class SpringBatchConfig {
     }
 
     @Bean
+    public Step moveFileStep() {
+        return new StepBuilder("move-step", jobRepository)
+                .tasklet(moveFileTasklet(), transactionManager)
+                .build();
+    }
+
+    private Tasklet moveFileTasklet() {
+        return (contribution, chunkContext) -> {
+            Path sourcePath = Paths.get("src/main/resources/unprocessed/enterprise-survey-csv.csv");
+            Path destinationPath = Paths.get("src/main/resources/processed/enterprise-survey-csv.csv");
+
+            Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
     public Job runJob() {
         return new JobBuilder("importCustomers", jobRepository)
-                .flow(step1())
+                .flow(processFileStep())
+                .next(moveFileStep())
                 .end()
                 .build();
     }
